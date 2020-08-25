468f3a2586e97fce57420926d06b75ed648599f3
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-08-25 18:50:58 +0100

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+// for css and js, use public folder
+app.use(express.static("public");
+
+// put html in views
+app.get("/", (req, res) {
+res.sendFile(__dirname + "/views/index.html");
+}
+
+app.listen(process.env.PORT, () => { 
+console.log("Your app is listening on port " + listener.address(). port); 
+}); 
