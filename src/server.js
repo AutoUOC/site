87fcbf4de12d80ee4d5e93e011cb9496c941e88f
SausageMcSauce <69243388+SausageMcSauce@users.noreply.github.com>
2020-08-26 09:30:57 +0000

    Fixed port

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.get("/", (req, res) => {
 res.sendFile(__dirname + "/views/index.html");
 });
 
-app.listen(process.env.PORT, () => { 
-console.log("Your app is listening on port " + listener.address(). port); 
+const port = process.env.PORT || 5000;
+app.listen(port, () => { 
+console.log("Your app is listening on port " + port); 
 }); 
