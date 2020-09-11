c4fe68df558f43cb9cc9ba66661b96e71edef745
LankyBox01 <66025173+LankyBox01@users.noreply.github.com>
2020-09-11 16:34:01 +0300

    Added warning
    I finally made some scripting XD
    I just added this warning for those who are dumb enough to paste random codes in their consoles

--- src/public/script.js
+++ src/public/script.js
@@ -1,1 +1,5 @@
 //this will probably just be lots of XmlHttpRequests (:
+
+//Warning for those who are dumb enough to paste random codes in their consoles
+console.group("Stop!");
+console.info("If someone told you to copy-paste a code here, DON'T DO IT! It would allow them to take over your account and disallow you from accessing our website!")
